ee2db8836f21b2552b4ba950a05b031ff1dcc2a9
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-09-06 19:28:53 +0530

    header aka navbar

--- second_project/src/main.jsx
+++ second_project/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import {ColorModeScript, ChakraProvider, theme} from "@chakra-ui/react"
+import ColorModeSwitcher from './ColorModeSwitcher.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <ColorModeSwitcher/>
+      <App />
+    </ChakraProvider>
+
   </StrictMode>,
 )

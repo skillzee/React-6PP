b15afe63efc57c8a083ea83af92202581885b159
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-09-06 18:32:35 +0530

    project one completed

--- first_project/src/App.jsx
+++ first_project/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Home from './components/Home'
 import Footer from './components/Footer'
 import Contact from './components/Contact'
+import Services from './components/Services'
 
 
 import "./styles/App.scss"
@@ -11,6 +12,7 @@ import "./styles/header.scss"
 import "./styles/home.scss"
 import "./styles/footer.scss"
 import "./styles/contact.scss"
+import "./styles/mediaquery.scss"
 
 const App = () => {
   return (
@@ -19,7 +21,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/contact' element={<Contact/>}/>
-        <Route path='/services' element={<Contact/>}/>
+        <Route path='/services' element={<Services/>}/>
 
       </Routes>
       <Footer/>
